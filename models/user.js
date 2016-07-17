873268c11858bcd54604df8b60581a255d513f3c
William T <william.tam18@gmail.com>
2016-07-17 15:40:45 +0800

    added authentication to user schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const uuid = require('uuid')
 
 const userSchema = new mongoose.Schema({
   id: Number,
@@ -8,6 +10,29 @@ const userSchema = new mongoose.Schema({
   password: {type: String, required: true},
   auth_token: {type: String, unique: true}
 })
+
+userSchema.pre('save', function (next) {
+  const user = this
+
+  if (!user.isModified('password')) return next()
+
+  bcrypt.genSalt(8, function (err, salt) {
+    if (err) return next(err)
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err)
+
+      user.password = hash
+      next()
+    })
+  })
+
+  user.auth_token = uuid.v4()
+})
+
+userSchema.methods.authenticate = function (password, callback) {
+  bcrypt.compare(password, this.password, callback)
+}
 
 const User = mongoose.model('User', userSchema)
 
