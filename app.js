bbd1c6f1fd95441567db6d07179cc804b1c8d15c
William T <william.tam18@gmail.com>
2016-07-18 13:14:07 +0800

    code cleanup

--- app.js
+++ app.js
@@ -25,16 +25,6 @@ app.use(function (req, res, next) {
   next()
 })
 
-/* POST new user */
-// app.post('/signup', (req, res) => {
-//   const user = new User(req.body.user)
-//   console.log(req.body.user)
-//   user.save((err, user) => {
-//     if (err) return res.status(401).json({error: err.message})
-//     res.status(201).json({message: 'User created.', auth_token: user.auth_token})
-//   })
-// })
-
 // let bob = new User({
 //   first_name: 'Bob',
 //   last_name: 'Segel',
