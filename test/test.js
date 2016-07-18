53c69879979ad7aea90cd464a30316b27e30a493
William T <william.tam18@gmail.com>
2016-07-18 13:54:30 +0800

    mocha will clear db and seed data

--- test/test.js
+++ test/test.js
@@ -4,8 +4,28 @@ const expect = require('chai').expect
 const supertest = require('supertest')
 const api = supertest('http://localhost:3000')
 const app = require('../app')
+const User = require('../models/user')
+const Resources = require('../models/resources')
+
+users = [
+  {first_name: 'Justin', last_name: 'Chan', email: 'juschanuk@gmail.com', password: 'accounting'},
+  {first_name: 'William', last_name: 'Tam', email: 'william.tam@gmail.com', password: 'cat'}
+]
+
+resources = [
+  {title: 'Title 1', url: 'http://www.bbc.co.uk', tags: ['news', 'UK'], user: users[0].id },
+  {title: 'Title 2', url: 'http://www.cnn.com', tags: ['news', 'USA'], user: users[1].id },
+  {title: 'Title 3', url: 'http://www.todayonline.com', tags: ['news', 'Singapore'], user: users[0].id },
+  {title: 'Title 4', url: 'http://www.dailymail.co.uk', tags: ['news', 'UK'], user: users[1].id },
+  {title: 'Title 5', url: 'http://www.mrbrown.com', tags: ['satire', 'Singapore'], user: users[0].id }
+]
 
 describe('GET /', () => {
+  before ((done) => {
+    User.find().remove((err) => console.log('delete all users'))
+    Resources.find().remove((err) => console.log('delete all resources'))
+    done()
+  })
   it('should return a 200 response', (done) => {
     api.get('/')
     .set('Accept', 'application/html')
@@ -15,21 +35,34 @@ describe('GET /', () => {
 
 describe('POST /register', function() {
   this.timeout(10000)
+
   it('should return "User created." message', (done) => {
     api.post('/register')
     .set('Accept', 'application/html')
-    .send({first_name: 'Justin', last_name: 'Chan', email: 'juschanuk@gmail.com', password: 'accounting'})
+    .send(users[0])
     .expect(201)
     .end( (err, response) => {
       expect(response.body.message).to.equal('User created.')
       done()
-    })
+      })
+  })
+
+  it('should return "User created." message', (done) => {
+    api.post('/register')
+    .set('Accept', 'application/html')
+    .send(users[1])
+    .expect(201)
+    .end( (err, response) => {
+      expect(response.body.message).to.equal('User created.')
+      done()
+      })
   })
 })
 
 describe('POST /resources', () => {
   it('should return a 200 response', (done) => {
     api.post('/resources')
+    .send({})
     .set('Accept', 'application/html')
     .expect(200, done)
   })
