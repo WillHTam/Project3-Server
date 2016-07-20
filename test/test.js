f0b11aa45ce0ae892c9be6b334d8c3f3f757b8f0
William T <william.tam18@gmail.com>
2016-07-20 16:35:37 +0800

    one test

--- test/test.js
+++ test/test.js
@@ -26,11 +26,11 @@ resources = [
 ]
 
 describe('GET /', () => {
-  before ((done) => {
-    User.find().remove((err) => console.log('delete all users'))
-    Resources.find().remove((err) => console.log('delete all resources'))
-    done()
-  })
+  // before ((done) => {
+  //   User.find().remove((err) => console.log('delete all users'))
+  //   Resources.find().remove((err) => console.log('delete all resources'))
+  //   done()
+  // })
   it('should return a 200 response', (done) => {
     api.get('/')
     .set('Accept', 'application/html')
@@ -38,257 +38,257 @@ describe('GET /', () => {
   })
 })
 
-describe('POST /register', function() {
-  this.timeout(10000)
-
-  it('should return "User created." message', (done) => {
-    api.post('/register')
-    .set('Accept', 'application/html')
-    .send(users[0])
-    .expect(201)
-    .end( (err, response) => {
-      expect(response.body.message).to.equal('User created.')
-      done()
-      })
-  })
-
-  it('should return "User created." message', (done) => {
-    api.post('/register')
-    .set('Accept', 'application/html')
-    .send(users[1])
-    .expect(201)
-    .end( (err, response) => {
-      expect(response.body.message).to.equal('User created.')
-      done()
-      })
-  })
-
-  it('should return "User created." message', (done) => {
-    api.post('/register')
-    .set('Accept', 'application/html')
-    .send(users[2])
-    .expect(201)
-    .end( (err, response) => {
-      expect(response.body.message).to.equal('User created.')
-      done()
-      })
-  })
-})
-
-describe('POST /resources', function() {
-  this.timeout(10000000)
-
-  it('should return a 200 response 1', (done) => {
-    api.post('/resources')
-      .send(resources[0])
-      .set('Accept', 'application/html')
-      .set('email', 'juschanuk@gmail.com')
-      .expect(201, done)
-  })
-
-  it('should return a 200 response 2', function (done) {
-    api.post('/resources')
-      .send(resources[1])
-      .set('Accept', 'application/html')
-      .set('email', 'william.tam@gmail.com')
-      .expect(201, done)
-  })
-
-  it('should return a 200 response 3', (done) => {
-    api.post('/resources')
-      .send(resources[2])
-      .set('Accept', 'application/html')
-      .set('email', 'juschanuk@gmail.com')
-      .expect(201, done)
-  })
-
-  it('should return a 200 response 4', (done) => {
-    api.post('/resources')
-      .send(resources[3])
-      .set('Accept', 'application/html')
-      .set('email', 'william.tam@gmail.com')
-      .expect(201, done)
-  })
-
-  it('should return a 200 response 5', (done) => {
-    api.post('/resources')
-      .send(resources[4])
-      .set('Accept', 'application/html')
-      .set('email', 'juschanuk@gmail.com')
-      .expect(201, done)
-  })
-
-  it('should return a 200 response 6', (done) => {
-    api.post('/resources')
-      .send(resources[5])
-      .set('Accept', 'application/html')
-      .set('email', 'angel@angel.com')
-      .expect(201, done)
-  })
-
-  it('should return a 401 response', (done) => {
-    api.post('/resources')
-    .send(resources[6])
-    .set('Accept', 'application/html')
-    .expect(401, done)
-  })
-
-  it('should return a 401 response', (done) => {
-    api.post('/resources')
-    .send(resources[6])
-    .set('Accept', 'application/html')
-    .set('email', 'blah@blah.com')
-    .expect(401, done)
-  })
-
-  it('should return a 200 response 7', (done) => {
-    api.post('/resources')
-      .send(resources[6])
-      .set('Accept', 'application/html')
-      .set('email', 'angel@angel.com')
-      .expect(201, done)
-  })
-
-  it('should return a 200 response 8', (done) => {
-    api.post('/resources')
-      .send(resources[7])
-      .set('Accept', 'application/html')
-      .set('email', 'angel@angel.com')
-      .expect(201, done)
-  })
-})
-
-describe('GET /allresources', () => {
-  it('should return a 200 response', (done) => {
-    api.get('/allresources')
-    .set('Accept', 'application/html')
-    .expect(200, done)
-  })
-})
-
-describe('GET /resources', () => {
-  it('should return a 200 response', (done) => {
-    User.findOne({email: users[0].email}, (err, user) => {
-      if (err) res.status(401).json({error: 'error'})
-      api.get('/resources')
-      .set('Accept', 'application/html')
-      .set('email', users[0].email)
-      .set('auth_token', user.auth_token)
-      .expect(200, done)
-    })
-  })
-})
-
-describe('POST /login', () => {
-  it('should return a 200 response and auth_token', (done) => {
-    var user = {email: users[0].email, password: users[0].password}
-    api.post('/login')
-    .set('Accept', 'application/html')
-    .send(user)
-    .expect(200)
-    .end( (err, response) => {
-      expect(response.body.message).to.equal('User logged in')
-      expect(response.body.auth_token).to.exist
-      done()
-      })
-  })
-})
-
-describe('DELETE /resources', () => {
-  it('should remove a resource', (done) => {
-    User.findOne({email: users[0].email}, (err, user) => {
-      Resources.findOne({user}, (err, resource) => {
-        if (err) res.status(422).json({message: 'Error finding resource'})
-        else {
-          var resourceID = resource._id
-          api.delete('/resources')
-          .send({id: resourceID})
-          .set('Accept', 'application/html')
-          .expect(401)
-          .end( (err, response) => {
-            expect(response.body.error).to.equal('User not found (deleteResource)')
-            done()
-          })
-        }
-      })
-    })
-  })
-
-  it('should remove a resource', (done) => {
-    User.findOne({email: users[0].email}, (err, user) => {
-      Resources.findOne({user}, (err, resource) => {
-        if (err) res.status(422).json({message: 'Error finding resource'})
-        else {
-          var resourceID = resource._id
-          api.delete('/resources')
-          .send({id: resourceID})
-          .set('Accept', 'application/html')
-          .set('email', users[0].email)
-          .set('auth_token', user.auth_token)
-          .expect(200)
-          .end( (err, response) => {
-            expect(response.body.message).to.equal('Resource deleted')
-            done()
-          })
-        }
-      })
-    })
-  })
-
-})
-
-describe('DELETE /deleteUser', () => {
-  it('should remove a user', (done) => {
-    User.findOne({email: users[1].email}, (err, user) => {
-      if (err) res.status(422).json({message: 'Error deleting user'})
-      else {
-        api.delete('/deleteUser')
-        .send({user})
-        .set('Accept', 'application/html')
-        .expect(200)
-        .end((err, response) => {
-          expect(response.body.message).to.equal('User and Resources deleted')
-          done()
-        })
-      }
-    })
-  })
-})
-
-describe('PUT /user', function () {
-  it('should edit the user', (done) => {
-    User.findOne({email: users[0].email}, (err, user) => {
-      if (err) res.status(401).json({error: 'error'})
-      var user1 = {first_name: 'Brian', last_name: 'Lopez', email: 'blopez@gmail.com', password: 'mexico'}
-      api.put('/user')
-      .set('Accept', 'application/html')
-      .set('email', user.email)
-      .set('auth_token', user.auth_token)
-      .send(user1)
-      .expect(200)
-      .end((err, response) => {
-        expect(response.body.message).to.equal('User successfully updated')
-        done()
-        })
-    })
-  })
-})
-
-describe('PUT /resources', function () {
-  it('should edit the resource', (done) => {
-    User.findOne({email: users[2].email}, (err, user) => {
-      Resources.findOne({user}, (err, resource) => {
-        if (err) res.status(401).json({error: 'error'})
-        var resource1 = {id: resource._id, title: 'Replacement Title', url: 'http://edition.cnn.com/2016/07/18/asia/north-korea-missiles/index.html', site_name: 'test.com', summary: 'EDITED ENTRY'}
-        api.put('/resources')
-        .set('Accept', 'application/html')
-        .send(resource1)
-        .expect(200)
-        .end((err, response) => {
-          expect(response.body.message).to.equal('Resource updated')
-          done()
-        })
-      })
-    })
-  })
-})
+// describe('POST /register', function() {
+//   this.timeout(10000)
+//
+//   it('should return "User created." message', (done) => {
+//     api.post('/register')
+//     .set('Accept', 'application/html')
+//     .send(users[0])
+//     .expect(201)
+//     .end( (err, response) => {
+//       expect(response.body.message).to.equal('User created.')
+//       done()
+//       })
+//   })
+//
+//   it('should return "User created." message', (done) => {
+//     api.post('/register')
+//     .set('Accept', 'application/html')
+//     .send(users[1])
+//     .expect(201)
+//     .end( (err, response) => {
+//       expect(response.body.message).to.equal('User created.')
+//       done()
+//       })
+//   })
+//
+//   it('should return "User created." message', (done) => {
+//     api.post('/register')
+//     .set('Accept', 'application/html')
+//     .send(users[2])
+//     .expect(201)
+//     .end( (err, response) => {
+//       expect(response.body.message).to.equal('User created.')
+//       done()
+//       })
+//   })
+// })
+//
+// describe('POST /resources', function() {
+//   this.timeout(10000000)
+//
+//   it('should return a 200 response 1', (done) => {
+//     api.post('/resources')
+//       .send(resources[0])
+//       .set('Accept', 'application/html')
+//       .set('email', 'juschanuk@gmail.com')
+//       .expect(201, done)
+//   })
+//
+//   it('should return a 200 response 2', function (done) {
+//     api.post('/resources')
+//       .send(resources[1])
+//       .set('Accept', 'application/html')
+//       .set('email', 'william.tam@gmail.com')
+//       .expect(201, done)
+//   })
+//
+//   it('should return a 200 response 3', (done) => {
+//     api.post('/resources')
+//       .send(resources[2])
+//       .set('Accept', 'application/html')
+//       .set('email', 'juschanuk@gmail.com')
+//       .expect(201, done)
+//   })
+//
+//   it('should return a 200 response 4', (done) => {
+//     api.post('/resources')
+//       .send(resources[3])
+//       .set('Accept', 'application/html')
+//       .set('email', 'william.tam@gmail.com')
+//       .expect(201, done)
+//   })
+//
+//   it('should return a 200 response 5', (done) => {
+//     api.post('/resources')
+//       .send(resources[4])
+//       .set('Accept', 'application/html')
+//       .set('email', 'juschanuk@gmail.com')
+//       .expect(201, done)
+//   })
+//
+//   it('should return a 200 response 6', (done) => {
+//     api.post('/resources')
+//       .send(resources[5])
+//       .set('Accept', 'application/html')
+//       .set('email', 'angel@angel.com')
+//       .expect(201, done)
+//   })
+//
+//   it('should return a 401 response', (done) => {
+//     api.post('/resources')
+//     .send(resources[6])
+//     .set('Accept', 'application/html')
+//     .expect(401, done)
+//   })
+//
+//   it('should return a 401 response', (done) => {
+//     api.post('/resources')
+//     .send(resources[6])
+//     .set('Accept', 'application/html')
+//     .set('email', 'blah@blah.com')
+//     .expect(401, done)
+//   })
+//
+//   it('should return a 200 response 7', (done) => {
+//     api.post('/resources')
+//       .send(resources[6])
+//       .set('Accept', 'application/html')
+//       .set('email', 'angel@angel.com')
+//       .expect(201, done)
+//   })
+//
+//   it('should return a 200 response 8', (done) => {
+//     api.post('/resources')
+//       .send(resources[7])
+//       .set('Accept', 'application/html')
+//       .set('email', 'angel@angel.com')
+//       .expect(201, done)
+//   })
+// })
+//
+// describe('GET /allresources', () => {
+//   it('should return a 200 response', (done) => {
+//     api.get('/allresources')
+//     .set('Accept', 'application/html')
+//     .expect(200, done)
+//   })
+// })
+//
+// describe('GET /resources', () => {
+//   it('should return a 200 response', (done) => {
+//     User.findOne({email: users[0].email}, (err, user) => {
+//       if (err) res.status(401).json({error: 'error'})
+//       api.get('/resources')
+//       .set('Accept', 'application/html')
+//       .set('email', users[0].email)
+//       .set('auth_token', user.auth_token)
+//       .expect(200, done)
+//     })
+//   })
+// })
+//
+// describe('POST /login', () => {
+//   it('should return a 200 response and auth_token', (done) => {
+//     var user = {email: users[0].email, password: users[0].password}
+//     api.post('/login')
+//     .set('Accept', 'application/html')
+//     .send(user)
+//     .expect(200)
+//     .end( (err, response) => {
+//       expect(response.body.message).to.equal('User logged in')
+//       expect(response.body.auth_token).to.exist
+//       done()
+//       })
+//   })
+// })
+//
+// describe('DELETE /resources', () => {
+//   it('should remove a resource', (done) => {
+//     User.findOne({email: users[0].email}, (err, user) => {
+//       Resources.findOne({user}, (err, resource) => {
+//         if (err) res.status(422).json({message: 'Error finding resource'})
+//         else {
+//           var resourceID = resource._id
+//           api.delete('/resources')
+//           .send({id: resourceID})
+//           .set('Accept', 'application/html')
+//           .expect(401)
+//           .end( (err, response) => {
+//             expect(response.body.error).to.equal('User not found (deleteResource)')
+//             done()
+//           })
+//         }
+//       })
+//     })
+//   })
+//
+//   it('should remove a resource', (done) => {
+//     User.findOne({email: users[0].email}, (err, user) => {
+//       Resources.findOne({user}, (err, resource) => {
+//         if (err) res.status(422).json({message: 'Error finding resource'})
+//         else {
+//           var resourceID = resource._id
+//           api.delete('/resources')
+//           .send({id: resourceID})
+//           .set('Accept', 'application/html')
+//           .set('email', users[0].email)
+//           .set('auth_token', user.auth_token)
+//           .expect(200)
+//           .end( (err, response) => {
+//             expect(response.body.message).to.equal('Resource deleted')
+//             done()
+//           })
+//         }
+//       })
+//     })
+//   })
+//
+// })
+//
+// describe('DELETE /deleteUser', () => {
+//   it('should remove a user', (done) => {
+//     User.findOne({email: users[1].email}, (err, user) => {
+//       if (err) res.status(422).json({message: 'Error deleting user'})
+//       else {
+//         api.delete('/deleteUser')
+//         .send({user})
+//         .set('Accept', 'application/html')
+//         .expect(200)
+//         .end((err, response) => {
+//           expect(response.body.message).to.equal('User and Resources deleted')
+//           done()
+//         })
+//       }
+//     })
+//   })
+// })
+//
+// describe('PUT /user', function () {
+//   it('should edit the user', (done) => {
+//     User.findOne({email: users[0].email}, (err, user) => {
+//       if (err) res.status(401).json({error: 'error'})
+//       var user1 = {first_name: 'Brian', last_name: 'Lopez', email: 'blopez@gmail.com', password: 'mexico'}
+//       api.put('/user')
+//       .set('Accept', 'application/html')
+//       .set('email', user.email)
+//       .set('auth_token', user.auth_token)
+//       .send(user1)
+//       .expect(200)
+//       .end((err, response) => {
+//         expect(response.body.message).to.equal('User successfully updated')
+//         done()
+//         })
+//     })
+//   })
+// })
+//
+// describe('PUT /resources', function () {
+//   it('should edit the resource', (done) => {
+//     User.findOne({email: users[2].email}, (err, user) => {
+//       Resources.findOne({user}, (err, resource) => {
+//         if (err) res.status(401).json({error: 'error'})
+//         var resource1 = {id: resource._id, title: 'Replacement Title', url: 'http://edition.cnn.com/2016/07/18/asia/north-korea-missiles/index.html', site_name: 'test.com', summary: 'EDitED ENTRY'}
+//         api.put('/resources')
+//         .set('Accept', 'application/html')
+//         .send(resource1)
+//         .expect(200)
+//         .end((err, response) => {
+//           expect(response.body.message).to.equal('Resource updated')
+//           done()
+//         })
+//       })
+//     })
+//   })
+// })
