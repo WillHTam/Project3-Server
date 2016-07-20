2a90292a12f4b9ed8f97630770f1e371117006f1
William T <william.tam18@gmail.com>
2016-07-20 19:27:41 +0800

    allow CORS without extension for everyone

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const User = require('./models/user')
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, email, auth_token, id')
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
   next()
 })
 
