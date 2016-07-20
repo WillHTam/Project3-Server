44cfa8eceacf4e2b020217010dc12189b002dc4a
William T <william.tam18@gmail.com>
2016-07-20 11:25:56 +0800

    moved relevant functions out of routes into controller

--- routes/index.js
+++ routes/index.js
@@ -15,50 +15,21 @@ router.get('/', function (req, res, next) {
 })
 
 /* POST new user */
-router.post('/register', (req, res) => {
-  const user = new User(req.body)
-  user.save((err, user) => {
-    if (err) return res.status(401).json({error: 'ERROR! Could not create user.'})
-    res.status(201).json({message: 'User created.'}) // ,  auth_token: user.auth_token})
-  })
-})
+router.post('/register', applicationController.userRegister)
 
 /* EDIT user */
 router.put('/user', applicationController.editUser)
 
 // User LOGIN
-router.post('/login', (req, res) => {
-  const userParams = new User(req.body)
-  User.findOne({email: userParams.email}, (err, user) => {
-    if (err || !user) return res.status(405).json({error: 'Cannot find user'})
-    user.authenticate(userParams.password, (err, isMatch) => {
-      if (err || !isMatch) return res.status(401).json({error: 'Password no match'})
-
-      res.status(200).json({message: 'User logged in', email: user.email, auth_token: user.auth_token})
-    })
-  })
-})
+router.post('/login', applicationController.userLogIn)
 
 // posts !!AND!! user DELETE
-router.delete('/deleteUser', (req, res) => {
-  console.log("In delete User")
-  const userEmail = req.body.user.email
-  console.log("Delete User, UserParams: " + userEmail)
-  User.findOne({email: userEmail}, (err, user) => {
-    if (err || !user) return res.status(401).json({error: 'Email or password is invalid'})
+router.delete('/deleteUser', applicationController.deleteUser)
 
-    // user.authenticate(userParams.password, (err, isMatch) => {
-    // if (err || !isMatch) return res.status(401).json({error: 'Email or password is invalid'})
-    Resource.find({user}).remove().exec()
-    user.remove()
-    res.status(200).json({message: 'User and Resources deleted'})
-  })
- // })
-})
-
+// GET all resources
 router.get('/allresources', resourceController.showAllResources)
 
-// VIEW MY resources
+// VIEW the logged in user's resources ONLY
 // router.get('/resources', applicationController.userLoggedIn, resourceController.seeMyResources)
 router.get('/resources', resourceController.seeMyResources)
 
@@ -72,9 +43,5 @@ router.route('/resources').put(resourceController.updateResource)
 
 // DELETE resource
 router.delete('/resources', resourceController.deleteResource)
-
-router.get('/secret', applicationController.userLoggedIn, (req, res) => {
-  res.status(200).json({secret: 'hello ' + req.currentUser.email})
-})
 
 module.exports = router
